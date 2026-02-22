8c32314eab35b006d63d3ca2a1d12f764b5fd67e
arennazaryan <166433257+arennazaryan@users.noreply.github.com>
2026-02-22 19:32:46 +0400

    Refactor download route to use new download info

--- app/routes/downloads.py
+++ app/routes/downloads.py
@@ -1,21 +1,18 @@
 from flask import Blueprint, send_file, flash, redirect, url_for
-from app.services.cache_service import get_dataset_path
+from app.services.cache_service import get_dataset_download_info
 
 downloads_bp = Blueprint("downloads", __name__)
 
-
 @downloads_bp.route("/download/<task_id>")
 def download_csv(task_id):
-    # Use the new function from cache_service
-    path = get_dataset_path(task_id)
+    download_info = get_dataset_download_info(task_id)
 
-    if not path:
-        flash("File expired or deleted.", "danger")
-        return redirect(url_for("home.home"))
+    if not download_info or download_info.get("type") != "file":
+        flash("File expired or deleted by the server reset.", "danger")
+        return redirect(url_for("home.index"))
 
-    # Force download the file
     return send_file(
-        path,
+        download_info["path"],
         mimetype="text/csv",
         as_attachment=True,
         download_name=f"cpc_data_{task_id}.csv"
